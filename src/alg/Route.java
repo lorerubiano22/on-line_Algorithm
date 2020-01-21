f2e88926581367b95b8833844837b367dcdb457a
Lorena <Lorena@141.244.95.205>
2020-01-21 11:30:34 +0100

    cleaning Route and Solution classes

--- src/alg/Route.java
+++ src/alg/Route.java
@@ -1,46 +1,26 @@
 package alg;
 
-import java.io.Serializable;
 import java.util.LinkedList;
 import java.util.List;
 
-public class Route implements Serializable, Comparable<Route> {
-	/* INSTANCE FIELDS & CONSTRUCTOR */
+public class Route {
 
-	private double coverage = 0.0; // route total costs
 	private double time = 0.0; // route total travel time
 	private double distance = 0.0; // route total travel distance
-	private float demand = 0.0F; // route total demand
-	private LinkedList<Edge> edges; // edges list
-	private float[] center; // (x-bar, y-bar) for all (x,y) in the route
-	private float getYRouteCenter = 0.0F;
-	private float getXRouteCenter = 0.0F;
-	private double score = 0.0;
-	private int Lastconnections = 0;
+		private LinkedList<Edge> edges; // edges list
+
 
 	// Constructor
 	public Route() {
 		edges = new LinkedList<Edge>();
-		center = new float[2];
 	}
 
 	public Route(Route r) {
-
-		center = new float[2];
-		this.coverage = r.coverage;
-		this.time = r.time;
-		this.distance = r.distance;
-		this.demand = r.demand;
-		this.center = r.center;
-		this.getYRouteCenter = r.getYRouteCenter;
-		this.getXRouteCenter = r.getXRouteCenter;
-		this.score = r.score;
-		this.Lastconnections = r.getLastconnections();
 		edges = new LinkedList<Edge>();
 		LinkedList<Edge> edges;
 		for (Edge e : r.getEdges()) {
 			Edge eAux = new Edge(e);
-			this.edges.add(e);
+			this.edges.add(eAux);
 		}
 
 	}
@@ -54,47 +34,18 @@ public class Route implements Serializable, Comparable<Route> {
 		distance = c;
 	}
 
-	public void setCoverage(double c) {
-		coverage = c;
-	}
-
-	public void setDemand(float d) {
-		demand = d;
-	}
-
-	public void setCenter(float[] coord) {
-		center = coord;
-	}
 
 	public void setEdges(LinkedList<Edge> e) {
 		edges = e;
 	}
 
-	public void setXRouteCenter(float getXRouteCenter) {
-		this.getXRouteCenter = getXRouteCenter;
-	}
 
-	public void setYRouteCenter(float getYRouteCenter) {
-		this.getYRouteCenter = getYRouteCenter;
-	}
-
-	public void setScore(double score) {
-		this.score = score;
-	}
 
 	public void addEdge(int i, Edge e) {
 		Edge eAux = new Edge(e);
 		this.edges.add(i, e);
 	}
 
-	public void setLastconnections(int connection) {
-		this.Lastconnections = connection;
-	}
-
-	/* LIST GET METHODS */
-	public double getcoverage() {
-		return coverage;
-	}
 
 	public double getTime() {
 		return time;
@@ -104,29 +55,12 @@ public class Route implements Serializable, Comparable<Route> {
 		return distance;
 	}
 
-	public float getDemand() {
-		return demand;
-	}
 
-	public float[] getCenter() {
-		return center;
-	}
 
 	public List<Edge> getEdges() {
 		return edges;
 	}
 
-	public double getScore() {
-		return score;
-	}
-
-	public float getYRouteCenter() {
-		return getYRouteCenter;
-	}
-
-	public float getXRouteCenter() {
-		return getXRouteCenter;
-	}
 
 	public void removeEdge(Edge e) {
 		this.edges.remove(e);
@@ -136,9 +70,6 @@ public class Route implements Serializable, Comparable<Route> {
 		this.time = this.time - e.getTime();
 	}
 
-	public int getLastconnections() {
-		return Lastconnections;
-	}
 
 	/* AUXILIARY METHODS */
 	/**
@@ -157,78 +88,10 @@ public class Route implements Serializable, Comparable<Route> {
 	public String toString() {
 		String s = "";
 		s = s.concat("\nRute time: " + (this.getTime()));
-		s = s.concat("\nRuta demand:" + this.getDemand());
 		s = s.concat("\nRuta edges: " + this.getEdges());
 		return s;
 	}
 
-	@Override
-	public int compareTo(Route arg0) {
-		// TODO Auto-generated method stub
-		if (score < arg0.score)
-			return 1;
-		if (score > arg0.score)
-			return -1;
-		else
-			return 0;
-	}
-
-	public void sortRoute() {
-		int origen = 0;
-		int EdgeId = 0;
-		Edge FirstEdge = null;
-		Edge toInsert = null;
-		Node nOr = null;
-		Node nEnd = null;
-		LinkedList<Edge> edgesCopy = new LinkedList<Edge>();
-		LinkedList<Edge> route = new LinkedList<Edge>();
-		/* Coping edges list */
-		// for(Edge e:this.getEdges()) {
-		for (int e = 0; e < this.getEdges().size(); e++) {
-			nOr = this.getEdges().get(e).getOrigin();
-			nEnd = this.getEdges().get(e).getEnd();
-			toInsert = new Edge(nOr, nEnd);
-			edgesCopy.add(toInsert);
-			if (toInsert.getOrigin().getId() == origen) {
-				FirstEdge = new Edge(nOr, nEnd);
-				EdgeId = nEnd.getId();
-				route.addLast(FirstEdge);
-				edgesCopy.remove(e);
-			}
-		}
-		while (route.size() < this.getEdges().size()) {
-			EdgeId = route.getLast().getEnd().getId();
-			for (int i = edgesCopy.size() - 1; i > -1; i--) {
-				if (edgesCopy.get(i).getOrigin().getId() == EdgeId) {
-					nOr = edgesCopy.get(i).getOrigin();
-					nEnd = edgesCopy.get(i).getEnd();
-					toInsert = new Edge(nOr, nEnd);
-					route.addLast(toInsert);
-					edgesCopy.remove(i);
-				}
-			}
-		}
-		System.out.println("Initial order route");
-		for (Edge e : this.getEdges()) {
-
-			System.out.println(e.getOrigin().getId() + "," + e.getEnd().getId());
-		}
-
-		for (int i = this.getEdges().size() - 1; i > -1; i--) {
-			this.removeEdge(this.getEdges().get(i));
-		}
-		for (int i = 0; i < route.size(); i++) {
-			this.addEdge(i, route.get(i));
-
-		}
-
-		System.out.println("after sort order route");
-		for (Edge e : this.getEdges()) {
-
-			System.out.println(e.getOrigin().getId() + "," + e.getEnd().getId());
-		}
-
-	}
 
 	public void calcTime() {
 		double cost = 0;
@@ -246,15 +109,7 @@ public class Route implements Serializable, Comparable<Route> {
 		this.setDistance(cost);
 	}
 
-	public void calcCoverage() {
-		int c = 0;
-		for (Edge e : this.edges) {
-			if (e.getEnd().getId() != 0 && e.getEnd().getProfit() > 1 && e.getEnd().getId() != 0
-					&& e.getEnd().getId() != 0) {
-				c++;
-			}
-		}
-		this.setCoverage(c);
-	}
+
+
 
 }
--- src/alg/Solution.java
+++ src/alg/Solution.java
@@ -1,140 +1,23 @@
 package alg;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.util.Collections;
-import java.util.HashSet;
 import java.util.LinkedList;
-import java.util.List;
-import java.util.Set;
 
 public class Solution implements Cloneable {
 
 	/* INSTANCE FIELDS & CONSTRUCTOR */
-
-	private static long nInstances = 0; // number of instances
-	private long id; // solution ID
-	private double totalTime = 0.0; // solution total travel time
-	private double totalDistance = 0.0; // solution travel distance
-	private float servedDemand = 0.0F; // accum. demand served so far
-	private LinkedList<Route> routes; // list of routes in this solution
+	private double totalTime; // solution total travel time
+	private double totalDistance; // solution travel distance
+	private LinkedList<Route> routes; // list of routes in this solution - keep for the extension
 	private double timePC; // elapsed computational time (in seconds)
-	private double reliability = 1.0;
-	private int stochClientsNoServed = 0; // Clientes no servidos
-	private int clientsServed = 0;
-	private double percentTimesViolated = 0;
-	private double distanceViolated = 0;
-	private Set<Node> notUsedNodes; // Set of nodes that aren't used in the solution
-	private static final String LINE_SEPARATOR = System.lineSeparator();
-	public double Coverage;
 
 	public Solution() {
-		nInstances++;
-		id = nInstances;
-		routes = new LinkedList<Route>();
-		notUsedNodes = new HashSet<>();
+		totalTime = 0.0; // solution total travel time
+		totalDistance = 0.0; // solution travel distance
+		routes= new LinkedList<Route>(); // list of routes in this solution - keep for the extension
+		timePC = 0.0;  // elapse
 	}
 
-	public Solution(Set<Node> notUsedNodes) {
-		nInstances++;
-		id = nInstances;
-		routes = new LinkedList<Route>();
-		this.notUsedNodes = notUsedNodes;
-	}
-
-	// new constructor
-	public Solution(Solution sol) {
-		nInstances++;
-
-		id = nInstances;
-
-		totalTime = sol.totalTime;
-		totalDistance = sol.totalDistance;
-		servedDemand = sol.servedDemand;
-		timePC = sol.timePC;
-		Coverage = sol.Coverage;
-
-		this.routes = new LinkedList<Route>();
-
-		for (Route r : sol.getRoutes()) {
-
-			Route rAux = new Route(r);
-			this.routes.add(rAux);
-		}
-
-		this.notUsedNodes = new HashSet<>();
-		for (Node n : sol.notUsedNodes) {
-			// Node nAux = new Node (n);
-			this.notUsedNodes.add(n);
-		}
-
-		stochClientsNoServed = sol.stochClientsNoServed;
-		clientsServed = sol.clientsServed;
-		percentTimesViolated = sol.percentTimesViolated;
-		distanceViolated = sol.distanceViolated;
-		reliability = sol.reliability;
-	}
-
-	public Solution(Solution sol, LinkedList<Route> newroutes) {
-		nInstances++;
-		id = nInstances;
-
-		totalTime = sol.totalTime;
-		totalDistance = sol.totalDistance;
-		servedDemand = sol.servedDemand;
-
-		timePC = sol.timePC;
-		reliability = sol.reliability;
-		Coverage = sol.Coverage;
-		this.routes = new LinkedList<Route>();
-		for (Route r : newroutes) {
-			this.routes.add(r);
-		}
-	}
-
-	public Solution(LinkedList<Node> nonServed) {
-		nInstances++;
-		id = nInstances;
-		routes = new LinkedList<Route>();
-	}
-
-	public void addRoute(Route aRoute, Integer index) {
-		routes.add(index, (aRoute));
-	}
-
-	/**
-	 * Adds a a route to the Solution and updates the notUsedNodes set
-	 *
-	 * @param aRoute The route to add into the solution
-	 */
-	public void addRoute(Route aRoute) {
-		routes.add(aRoute);
-		int i = 0;
-		for (Edge e : aRoute.getEdges()) {
-			if (i != aRoute.getEdges().size() - 1)
-				notUsedNodes.remove(e.getEnd());
-			++i;
-		}
-	}
-
-	public void addRoutes(List<Route> l) {
-		routes.addAll(l);
-	}
-
-	public void deleteRoute(int i) {
-		routes.remove(i);
-	}
-
-	/**
-	 * It sorts the routes by cost and it keeps only the better ones that can be
-	 * visited with the number of vehicles available
-	 *
-	 * @param inputs Inputs instance that specifies the number of vehicles available
-	 */
 	public void updatingSolutionAttributes() {
-		Collections.sort(routes);
 		double totalCost = 0.0;
 		double distance = 0.0;
 		for (Route r : this.routes) {
@@ -145,34 +28,9 @@ public class Solution implements Cloneable {
 		setTime(totalCost);
 	}
 
-	public void setTotalCoverage(double totalCoverage) {
-		Coverage = totalCoverage;
-	}
-
-	/**
-	 * It removes the last n - topn routes
-	 *
-	 * @param topn Number of routes to keep
-	 */
-	public void sliceSolution(int topn) {
-		while (routes.size() != topn) {
-			Route r = routes.removeLast();
-			int i = 0;
-			for (Edge e : r.getEdges()) {
-				if (i != r.getEdges().size() - 1)
-					notUsedNodes.add(e.getEnd());
-				++i;
-			}
-		}
-	}
-
 	/* GET METHODS */
 	public LinkedList<Route> getRoutes() {
 		return routes;
-	}
-
-	public long getId() {
-		return id;
 	}
 
 	public double getTotalTime() {
@@ -183,40 +41,8 @@ public class Solution implements Cloneable {
 		return totalDistance;
 	}
 
-	public float getServedDemand() {
-		return servedDemand;
-	}
-
 	public double getPCTime() {
 		return timePC;
-	}
-
-	public double getReliability() {
-		return reliability;
-	}
-
-	public int getStochClientsNoServed() {
-		return stochClientsNoServed;
-	}
-
-	public int getClientsServed() {
-		return clientsServed;
-	}
-
-	public double getPercentTimesViolated() {
-		return percentTimesViolated;
-	}
-
-	public double getDistanceViolated() {
-		return distanceViolated;
-	}
-
-	public Set<Node> getNotUsedNodes() {
-		return notUsedNodes;
-	}
-
-	public double getTotalCoverage() {
-		return Coverage;
 	}
 
 	/* SET METHODS */
@@ -229,32 +55,8 @@ public class Solution implements Cloneable {
 		totalDistance = c;
 	}
 
-	public void setServedDemand(float d) {
-		servedDemand = d;
-	}
-
 	public void setPCTime(double t) {
 		timePC = t;
-	}
-
-	public void setReliability(double rel) {
-		reliability = rel;
-	}
-
-	public void setStochClientsNoServed(int stochClientsNoServed) {
-		this.stochClientsNoServed = stochClientsNoServed;
-	}
-
-	public void setClientsServed(int clientsServed) {
-		this.clientsServed = clientsServed;
-	}
-
-	public void setPercentTimesViolated(double percentTimesViolated) {
-		this.percentTimesViolated = percentTimesViolated;
-	}
-
-	public void setNotUsedNodes(Set<Node> notUsedNodes) {
-		this.notUsedNodes = notUsedNodes;
 	}
 
 	/* AUXILIARY METHODS */
@@ -263,35 +65,18 @@ public class Solution implements Cloneable {
 	public String toString() {
 		Route aRoute; // auxiliary Route variable
 		String s = "";
-		s = s.concat(LINE_SEPARATOR);
-		s = s.concat("Sol ID : " + getId() + LINE_SEPARATOR);
-
-		s = s.concat("Sol total time: " + getTotalTime() + LINE_SEPARATOR);
-		s = s.concat("Sol total distance: " + getTotalDistance() + LINE_SEPARATOR);
-		// s = s.concat("Sol total visited victims: " + getTotalCoverage() +
-		// LINE_SEPARATOR);
-		s = s.concat("Sol run time: " + getPCTime() + LINE_SEPARATOR);
-		s = s.concat(LINE_SEPARATOR);
-//		s = s.concat("Note: "+ LINE_SEPARATOR);
-//		s = s.concat("If last connection is: "+ LINE_SEPARATOR);
-		// s = s.concat("-1: aerial disrupted connection."+ LINE_SEPARATOR);
-		// s = s.concat("1: active connection."+ LINE_SEPARATOR);
-		// s = s.concat(LINE_SEPARATOR);
-
+		s = s.concat(System.lineSeparator());
+		s = s.concat("Sol total time: " + getTotalTime() + System.lineSeparator());
+		s = s.concat("Sol total distance: " + getTotalDistance() + System.lineSeparator());
+		s = s.concat("Sol run time: " + getPCTime() + System.lineSeparator());
+		s = s.concat(System.lineSeparator());
 		s = s.concat("\r\n\r\n\r\n");
 		s = s.concat("List of routes (cost and nodes): \r\n\r\n");
 		for (int i = 1; i <= routes.size(); i++) {
 			aRoute = routes.get(i - 1);
-
-//	if(aRoute.getLastconnections()==-1) {
-//		s = s.concat("AERIAL CONNECTION. Last insertions = DISRUPTED CONNECTION || ");
-//	}
-//	else {
-//		s = s.concat("Last insertions = ACTIVED CONNECTION || ");}
 			s = s.concat("Travel time = " + aRoute.getTime() + " || ");
 			s = s.concat("Travel distance = " + aRoute.getDistance() + " || ");
-			s = s.concat(LINE_SEPARATOR);
-
+			s = s.concat(System.lineSeparator());
 			s = s.concat("\n");
 			s = s.concat("Nodes = ");
 			int last = -1;
@@ -301,60 +86,7 @@ public class Solution implements Cloneable {
 			}
 			s = s.concat(last + "\n");
 		}
-
 		return s;
-	}
-
-	public static Object deepClone(Object object) {
-		try {
-			ByteArrayOutputStream baos = new ByteArrayOutputStream();
-			ObjectOutputStream oos = new ObjectOutputStream(baos);
-			oos.writeObject(object);
-			ByteArrayInputStream bais = new ByteArrayInputStream(baos.toByteArray());
-			ObjectInputStream ois = new ObjectInputStream(bais);
-			return ois.readObject();
-		} catch (Exception e) {
-			e.printStackTrace();
-			return null;
-		}
-	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + (int) (id ^ (id >>> 32));
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Solution other = (Solution) obj;
-		if (id != other.id)
-			return false;
-		return true;
-	}
-
-	public void setSolveconditions(double[][] tV, double[][] imp, int[][] cover) {
-
-		int n = tV.length;
-		double[][] traveltimes = new double[n][n];
-		double[][] importanceElements = new double[n][n];
-		int[][] elementsInNetwork = new int[n][n];
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < n; j++) {
-				traveltimes[i][j] = tV[i][j];
-				importanceElements[i][j] = imp[i][j];
-				elementsInNetwork[i][j] = cover[i][j];
-			}
-		}
-
 	}
 
 }
